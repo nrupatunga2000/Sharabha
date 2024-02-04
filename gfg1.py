e0a072b89e924555d62f6eb464c50851bfc8b22a
nrupatunga2000 <nrupatunga619@gmail.com>
2024-02-04 15:34:39 +0530

    simple GFG practice code

---
+++ gfg1.py
@@ -0,0 +1,13 @@
+#Print numbers from 1 to N without the help of loops.
+
+class Solution:
+
+    def printNos(self,N):
+
+        if N>0:
+            N = N-1
+
+        return print(N)
+
+obj1 = Solution()
+
