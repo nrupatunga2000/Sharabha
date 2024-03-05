e0cd2b6da19c40695f117e018612895caaf66b7c
nrupatunga2000 <nrupatunga619@gmail.com>
2024-03-05 12:42:25 +0530

    palindrom numbers

---
+++ palindrom.py
@@ -0,0 +1,14 @@
+#Given a Integer array A[] of n elements.
+# Your task is to complete the function PalinArray.
+# Which will return 1 if all the elements of the Array are palindrome
+# otherwise it will return 0.
+
+class Palindrom:
+    arr = [123,121,1332331]
+    def palinArray(self):
+        print()
+
+
+
+
+
