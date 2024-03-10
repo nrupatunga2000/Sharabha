170413b7f7fb079138a769cc0d69b0022f1a160c
nrupatunga2000 <nrupatunga619@gmail.com>
2024-03-10 14:22:32 +0530

    beauty calculation fun code

---
+++ beauty_code.py
@@ -0,0 +1,40 @@
+print('''Hi, can I ask you a queastion please?
+
+
+"are you a question paper?''')
+ans1 = input("hey say yes or no \n")
+ans1 = ans1.lower()
+if ans1 == "yes" or ans1 == "no":
+    print('''.
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    Oh! Sorry I was blank because of your beauty''')
+
+
