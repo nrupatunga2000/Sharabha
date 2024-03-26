5617d44d154ef9145a3909075b4d812d7f262635
Nrupatunga <57655312+nrupatunga2000@users.noreply.github.com>
2024-03-26 22:32:58 +0530

    Update cbt1.py

--- cbt1.py
+++ cbt1.py
@@ -1,7 +1,7 @@
 # Given a string, return a new string where the first and last chars have been exchanged.
 # front_back('code') → 'eodc'
 # front_back('a') → 'a'
-# front_back('ab') → 'ba
+# front_back('ab') → 'ba//
 
 def front_back(str):
     print(str[-1]+str[1:-1]+str[0])
