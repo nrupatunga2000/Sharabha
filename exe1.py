1548c1d1dff2175643b5f776741a3e1026df180c
nrupatunga2000 <nrupatunga619@gmail.com>
2024-04-03 11:24:02 +0530

    sum of current and previous num

---
+++ exe1.py
@@ -0,0 +1,9 @@
+#Print the sum of the current number and the previous number
+class Muddu:
+    def pingu(self,input_num):
+        sum = input_num+(input_num-1)
+        print(sum)
+object = Muddu()
+object.pingu(5)
+
+
---
+++ love_calculator.py
@@ -0,0 +1,4 @@
+print("search in google if you don't know")
+copper = input("enter the periodic symbol of copper: ")
+telurium = input("enter the periodic symbol of tellurium: ")
+print(f"you are {copper}{telurium}!!!:)cu")
