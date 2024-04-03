423bf388ccb669ef5d71b33eb06cc049e79028f9
nrupatunga2000 <nrupatunga619@gmail.com>
2024-04-03 12:35:09 +0530

    Print characters from a string that are present at an even index number

---
+++ exe3.py
@@ -0,0 +1,9 @@
+#Print characters from a string that are present at an even index number
+#For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
+string = 'Nrupatunga'
+n = 0
+while n<10:
+    print(string[n])
+    n+=2
+
+
