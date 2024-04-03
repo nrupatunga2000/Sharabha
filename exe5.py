9ca576716c5b9349761a6230ecaa1a0b71281e8b
nrupatunga2000 <nrupatunga619@gmail.com>
2024-04-03 12:46:14 +0530

    Remove first n characters from a string

---
+++ exe5.py
@@ -0,0 +1,5 @@
+#Remove first n characters from a string
+#remove_chars("pynative", 4) so output must be tive. Here, we need to remove the first four characters from a string.
+n = int(input("enter a number 3"))
+string ='Nrupatunga'
+print(string[n:])
