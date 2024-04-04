953ec3d27097c5d0c9bf50269c8fae5ca02cfc6e
nrupatunga2000 <nrupatunga619@gmail.com>
2024-04-04 19:22:06 +0530

    Display numbers divisible by 5 from a list

---
+++ exe4.py
@@ -0,0 +1,14 @@
+# Display numbers divisible by 5 from a list
+#Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+class Muddu:
+    def tanu(self):
+        series = [20,5,6,8,25]
+        for i in series:
+            if i%5 == 0:
+                print(i)
+            else:
+                pass
+
+object = Muddu()
+Muddu.tanu(input("just press enter:"))
