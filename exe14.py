31a98eebdfe6208b7de813d04de7cdbac8c0df11
nrupatunga2000 <nrupatunga619@gmail.com>
2024-04-09 10:36:45 +0530

    Exercise 14: Print a downward Half-Pyramid Pattern of Star (asterisk)

---
+++ exe14.py
@@ -0,0 +1,10 @@
+Exercise 14: Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+class Tanu:
+    def pingu(self):
+        for n in range(6,0,-1):
+            print('*'*n)
+            n = n-1
+papu = Tanu()
+papu.pingu()
+
