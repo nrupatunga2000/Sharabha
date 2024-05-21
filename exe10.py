6ee09ed5ae8bc2f74bb2288d6c6b48c6fa057952
Nrupatunga <57655312+nrupatunga2000@users.noreply.github.com>
2024-05-21 09:14:11 +0530

    Update exe10.py

--- exe10.py
+++ exe10.py
@@ -1,4 +1,4 @@
-# reate a new list from two list using the following condition
+# Create a new list from two list using the following condition
 #
 # Given two list of numbers, write a program to create a new list
 # such that the new list should contain odd numbers
