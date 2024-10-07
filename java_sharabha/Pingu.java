fc081a09de8aa743e7d1c7b55f3595152c58e5b5
Nrupatunga <nrupatunga619@gmail.com>
2024-10-07 17:54:33 +0530

    just a demo commit

---
+++ java_sharabha/Pingu.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Pingu {
+
+    public static void demo(String fname){
+        System.out.println(fname + "hajama");
+    }
+    
+}
+
+public static void main(String[]args){
+    demo("Gurlinga");
+    demo("samy");
+}
